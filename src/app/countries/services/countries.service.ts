1f208d2ea208394a8a9c6b6ed2c6077aae217d68
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2023-08-10 22:36:55 -0500

    finish app

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of, catchError } from 'rxjs';
+import { Observable, of, catchError, map } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({ providedIn: 'root' })
@@ -35,6 +35,17 @@ export class CountriesService {
       catchError((error) => {
         console.log(error);
         return of([]);
+      })
+    );
+  }
+
+  searchAlphaCode(code: string): Observable<Country | null> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Country[]>(url).pipe(
+      map((countries) => (countries.length > 0 ? countries[0] : null)),
+      catchError((error) => {
+        console.log(error);
+        return of(null);
       })
     );
   }
